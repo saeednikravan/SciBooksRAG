c04361aad1ec65d682ddd830e27aefa03d6fa7c9
saeed <nikravansaeed@gmail.com>
2026-06-21 12:43:25 +0330

    fix bug

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -1,4 +1,8 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { catchError, throwError } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   if (req.url.includes('/login')) return next(req);
@@ -6,5 +10,15 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
   if (token) {
     req = req.clone({ setHeaders: { Authorization: `Bearer ${token}` } });
   }
-  return next(req);
+  const router = inject(Router);
+  const auth = inject(AuthService);
+  return next(req).pipe(
+    catchError(err => {
+      if (err.status === 401) {
+        auth.logout();
+        router.navigate(['/login']);
+      }
+      return throwError(() => err);
+    })
+  );
 };
